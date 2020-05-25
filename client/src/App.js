a0cfe3d3d7bd6b2d8c363b478da8ac7f9e1eeac2
Trent Bennett <rehab.rtb@gmail.com>
2020-05-24 21:28:11 -0600

    adding client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import "./App.css";
+
+const GET_USERS = gql`
+  {
+    users {
+      id
+      login
+      avatar_url
+    }
+  }
+`;
+
+const User = ({ user: { login, avatar_url } }) => (
+  <div className="Card">
+    <div>
+      <img alt="avatar" className="Card--avatar" src={avatar_url} />
+      <h1 className="Card--name">{login}</h1>
+    </div>
+    <a href={`https://github.com/${login}`} className="Card--link">
+      See profile
+    </a>
+  </div>
+);
 
 function App() {
+  const { loading, error, data } = useQuery(GET_USERS);
+
+  if (error) return <h1>Something went wrong!</h1>;
+  if (loading) return <h1>Loading...</h1>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>Github | Users</h1>
+      {data.users.map((user) => (
+        <User key={user.id} user={user} />
+      ))}
+    </main>
   );
 }
 
